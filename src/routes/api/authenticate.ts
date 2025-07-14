cb4203654c2fd8d8a6efbf4e80d6289c1bbf8772
Jake Scott <jake.net@gmail.com>
2025-07-14 17:53:18 +1200

    authenticate

--- src/routes/api/authenticate.ts
+++ src/routes/api/authenticate.ts
@@ -9,69 +9,48 @@ export const ServerRoute = createServerFileRoute("/api/authenticate").methods({
     const stytch_token_type = url.searchParams.get("stytch_token_type");
     const token = url.searchParams.get("token");
 
-    if (stytch_token_type == "discovery_oauth" && token) {
-      const stytch = useStytch();
-      try {
+    const stytch = useStytch();
+    const session = await useAppSession();
+
+    let intermediateSessionToken: string;
+    let emailAddress: string;
+
+    try {
+      if (stytch_token_type == "discovery_oauth" && token) {
         const { intermediate_session_token, email_address } = await stytch.oauth.discovery.authenticate({
           discovery_oauth_token: token,
         });
 
-        const session = await useAppSession();
-
-        await session.update({
-          intermediateSessionToken: intermediate_session_token,
-          email: email_address,
-          sessionJwt: undefined,
-        });
-
-        return new Response("success", {
-          status: 307,
-          headers: {
-            location: "/discovery/select-organisation",
-          },
-        });
-      } catch (error) {
-        return new Response("failed", {
-          status: 307,
-          headers: {
-            location: "/",
-          },
-        });
-      }
-    } else if (stytch_token_type === "discovery" && token) {
-      const stytch = useStytch();
-
-      try {
+        intermediateSessionToken = intermediate_session_token;
+        emailAddress = email_address;
+      } else if (stytch_token_type === "discovery" && token) {
         const { intermediate_session_token, email_address } = await stytch.magicLinks.discovery.authenticate({
           discovery_magic_links_token: token,
         });
 
-        const session = await useAppSession();
-
-        await session.update({
-          intermediateSessionToken: intermediate_session_token,
-          email: email_address,
-          sessionJwt: undefined,
-        });
-
-        return new Response("success", {
-          status: 307,
-          headers: {
-            location: "/discovery/select-organisation",
-          },
-        });
-      } catch (error) {
-        // console.log("discovery authenticate failed");
-
-        return new Response("failed", {
-          status: 307,
-          headers: {
-            location: "/",
-          },
-        });
+        intermediateSessionToken = intermediate_session_token;
+        emailAddress = email_address;
+      } else {
+        throw new Error("Unhandle auth callback");
       }
-    } else {
-      throw new Error("Unhandle auth callback");
+    } catch (error) {
+      return new Response("failed", {
+        status: 307,
+        headers: { location: "/" },
+      });
     }
+
+    await session.update({
+      intermediateSessionToken: intermediateSessionToken,
+      email: emailAddress,
+      sessionJwt: undefined,
+    });
+
+    return new Response("success", {
+      status: 307,
+      headers: {
+        location: "/discovery/select-organisation",
+      },
+    });
   },
 });
