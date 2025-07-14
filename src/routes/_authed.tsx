92289cc9de821472ee4354b5b8d60ad82cd05c38
Jake Scott <jake.net@gmail.com>
2025-07-14 17:44:26 +1200

    sessions

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -1,8 +1,3 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { useAppSession } from "@/lib/session";
 import { useStytch } from "@/lib/stytch";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
@@ -21,30 +16,11 @@ const authenticate = createServerFn().handler(async () => {
 
   let memberSession: MemberSession | null = null;
   try {
-    memberSession = await stytch.sessions.authenticateJwtLocal({
-      session_jwt: session.data.sessionJwt,
-      max_token_age_seconds: parseInt(process.env.MAX_TOKEN_AGE_SECONDS!),
-    });
+    const authResult = await stytch.sessions.authenticateJwt({ session_jwt: session.data.sessionJwt });
+    memberSession = authResult.member_session;
+    await session.update({ sessionJwt: authResult.session_jwt });
   } catch (err) {
-    // console.log("JWT token local validation expired");
-  }
-
-  if (!memberSession) {
-    try {
-      const authResult = await stytch.sessions.authenticate({
-        session_jwt: session.data.sessionJwt,
-        session_duration_minutes: parseInt(process.env.SESSION_DURATION_MINUTES!),
-      });
-
-      memberSession = authResult.member_session;
-
-      await session.update({
-        sessionJwt: authResult.session_jwt,
-      });
-    } catch (err) {
-      // console.error("Could not find member by session token", err);
-      throw redirect({ to: "/" });
-    }
+    throw redirect({ to: "/" });
   }
 
   const sidebarState = await getCookie("sidebar_state");
